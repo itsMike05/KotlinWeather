c1bcca0895e0af8f258f599e0f1ed0011f162bee
Michał Pelc <michal.pelc@live.com>
2023-01-31 15:44:22 +0100

    Created the logo icon, added to the MainView

--- app/src/main/java/com/itsmike/weatherkotlin/MainActivity.kt
+++ app/src/main/java/com/itsmike/weatherkotlin/MainActivity.kt
@@ -3,14 +3,19 @@ package com.itsmike.weatherkotlin
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.itsmike.weatherkotlin.ui.theme.WeatherKotlinTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,7 +35,17 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun WeatherPage() {
 
-    WeatherIcon()
+    Column(
+        modifier = Modifier.fillMaxWidth()
+            .padding(
+                top = 30.dp,
+                start = 80.dp,
+                end = 80.dp
+            ),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        WeatherIcon()
+    }
 
 }
 
--- app/src/main/java/com/itsmike/weatherkotlin/WeatherIcon.kt
+++ app/src/main/java/com/itsmike/weatherkotlin/WeatherIcon.kt
@@ -1,10 +1,30 @@
 package com.itsmike.weatherkotlin
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.itsmike.weatherkotlin.ui.theme.WeatherKotlinTheme
 
 @Composable
 fun WeatherIcon() {
-    Image(painter = painterResource(id = R.drawable.ic_logo), contentDescription = "App Logo")
+    Image(
+        painter = painterResource(id = R.drawable.ic_logo),
+        contentDescription = "App Logo",
+        modifier = Modifier
+            .width(200.dp)
+    )
+}
+
+
+@Preview (showBackground = true)
+@Composable
+fun IconPreview() {
+    WeatherKotlinTheme() {
+        WeatherIcon()
+    }
 }
